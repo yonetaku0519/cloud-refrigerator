01affdf6c076bea09c22ab8bb38087310773d20a
yonetaku0519 <yonezawa051319@gmail.com>
2021-07-14 06:33:02 +0000

    migration

---
+++ database/migrations/2021_07_14_093254_create_foods_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('storing_id');
+            $table->string('name');
+            $table->string('amount');
+            $table->date('freshbess_date');
+            $table->string('note');
+            $table->integer('status');
+            $table->timestamps();
+            $table->softDeletes();
+            
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foods');
+    }
+}
