cd3757b5a6331d4368ea4f7e034159011f0cc453
yonetaku0519 <yonezawa051319@gmail.com>
2021-07-25 13:32:53 +0000

    Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(storingsTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
     }
 }
