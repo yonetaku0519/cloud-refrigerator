4354f168d8a6e0e0c525af026c1e8d3a0fbe51fc
yonetaku0519 <yonezawa051319@gmail.com>
2021-08-05 06:52:16 +0000

    make it better

--- resources/views/commons/footer.blade.php
+++ resources/views/commons/footer.blade.php
@@ -1,11 +1,11 @@
 <footer>
 　　<div id="footer_navi">
-        <ul>
+        <ul class = "footer_ul" style = "">
             <li><a href="{{ url('/') }}">TOP</a></li>
             <li><a href="{{ route('login') }}">ログイン</a></li>
             <li><a href="{{ route('register') }}">ユーザー登録</a></li>
             <li><a href="{{ route('contact') }}">お問い合わせ</a></li>
         </ul>
-    </div
+    </div>
      <small style = "display: block; padding: 8px 0;">&copy; 2021 クラウド冷蔵庫</small>
 </footer>
